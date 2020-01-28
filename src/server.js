508b7193f34f89591b1f8d7c7aa496f3ac7c3401
mitaksim <miriamt.simonnet@gmail.com>
2020-01-28 16:24:12 +0100

    Config express.json

--- src/server.js
+++ src/server.js
@@ -4,11 +4,14 @@ const express = require('express');
 // 2. Création de l'app avec express
 const app = express();
 
+// 5. Pour qu'express retourne en format json json
+app.use(express.json());
+
 // 4. Je définis la route où je veux éxecuter le code :
 // Premier paramètre: "/",
 // Deuxième paramètre: une fonction qui reçoit deux autres paramètres (req, res)
-app.get('/', (req, res) => {
-    return res.json({ message: 'Hello World' });
+app.post('/users', (req, res) => {
+    return res.json(req.body);
 });
 
 // 3. Port qui va écouter l'app
