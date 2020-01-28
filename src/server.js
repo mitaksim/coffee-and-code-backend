03b8f1cc5843ef5f050aa4d73c6638770213987d
mitaksim <miriamt.simonnet@gmail.com>
2020-01-28 14:51:57 +0100

    Config express

---
+++ src/server.js
@@ -0,0 +1,16 @@
+// 1. Import d'express
+const express = require('express');
+
+// 2. Création de l'app avec express
+const app = express();
+
+// 4. Je définis la route où je veux éxecuter le code :
+// Premier paramètre: "/",
+// Deuxième paramètre: une fonction qui reçoit deux autres paramètres (req, res)
+app.get('/', (req, res) => {
+    return res.send('Hello World');
+});
+
+// 3. Port qui va écouter l'app
+app.listen(3333);
+
