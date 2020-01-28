1fe3b254052bb3ecde0314207ee81be72ca97a5c
mitaksim <miriamt.simonnet@gmail.com>
2020-01-28 20:07:04 +0100

    Function if user does not exist OK

--- src/controllers/CoffeeController.js
+++ src/controllers/CoffeeController.js
@@ -1,14 +1,22 @@
 // Import du model Coffee
 const Coffee = require('../models/Coffee');
+// Import du model User
+const User = require('../models/User');
 
 module.exports = {
     async store(req, res) {
+        // Requêtes
         const { filename } = req.file;
-
         const { name, techs, price } = req.body;
-
         const { user_id } = req.headers;
+        const User = require('../models/User')
 
+        // Si l'utilisateur n'est pas inscrit, retourner un message d'erreur
+        const user = await User.findById(user_id);
+
+        if (!user) {
+            return res.status(400).json({ error: "L'utilisateur n'existe pas" });
+        }
         const coffee = await Coffee.create({
             user: user_id,
             thumbnail: filename,
@@ -17,6 +25,7 @@ module.exports = {
             price
         })
 
+        // Réponse
         return res.json({coffee});
     }
 }
