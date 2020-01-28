d2ca70e7d52758b0afbd43d43a9d3c32e302a899
mitaksim <miriamt.simonnet@gmail.com>
2020-01-28 19:30:07 +0100

    Routes to uploads and CoffeeController

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,20 @@
 // 1. Import express
 const express = require ('express');
-// Import de SessionController
+const multer = require('multer');
+
+const uploadConfig = require('./config/upload');
+// Import des controllers
 const SessionController = require('./controllers/SessionController');
+const CoffeeController = require('./controllers/CoffeeController');
 
 // 2. On appele la fonction Router d'express
 const routes = express.Router();
 
-// 3. On recupère la route crée dans server.js et je change app.post par routes.post
+const upload = multer(uploadConfig);
+
+// Routes
 routes.post('/sessions', SessionController.store);
+routes.post('/coffees', upload.single('thumbnail'), CoffeeController.store);
 
 // On doit exporter les routes pour qu'on ait accès dans app
 module.exports = routes;
